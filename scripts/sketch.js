3aa0ccade0b94609d03d832336de974d6b5369b6
Rafael Caro Repetto <rafael.caro.repetto@gmail.com>
2019-01-21 22:46:19 +0100

    Overall display similar to tal-following

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,6 +1,7 @@
 var extraSpaceH = 0;
 var extraSpaceW = 0;
 var mainSpace = 600;
+var margin = 10;
 
 var recordingsInfo;
 var recordingsList;
@@ -9,9 +10,21 @@ var maxHz;
 var pitchTrack;
 var trackFile;
 var track;
+var trackDuration;
+var title;
+var artist;
+var link;
 
 var select;
 var buttonPlay;
+
+var cursorTop;
+var cursorBottom;
+var navBoxH = 50;
+var navCursor;
+var navBox;
+var navCursorW = 4;
+var clock;
 
 var loaded = false;
 var paused = true;
@@ -36,9 +49,11 @@ function setup () {
 
   recordingsList = recordingsInfo["recordingsList"];
 
+  infoLink = select("#info-link");
+  infoLink.position(width-60, extraSpaceH + margin*3.5 + 30);
   select = createSelect()
     .size(120, 25)
-    .position(15, 15)
+    .position(margin, margin)
     .changed(start)
     .parent("sketch-holder");
   select.option("Elige");
@@ -52,24 +67,104 @@ function setup () {
   }
   buttonPlay = createButton("Carga el audio")
     .size(120, 25)
-    .position(extraSpaceW + 15, extraSpaceH + 55)
+    .position(width - 120 - margin, height - margin * 2 - navBoxH - 25)
     .mouseClicked(player)
     .attribute("disabled", "true")
     .parent("sketch-holder");
+
+  navBox = new createNavigationBox();
+  navCursor = new CreateNavCursor();
+
+  cursorTop = extraSpaceH + margin*6 + 50;
+  cursorBottom = buttonPlay.y-margin*2;
 }
 
 function draw () {
-  background(205, 92, 92);
+  background(240, 128, 128);
+
+  textAlign(CENTER, TOP);
+  textStyle(NORMAL);
+  textSize(30);
+  strokeWeight(5);
+  stroke(120, 0, 0);
+  fill(240, 128, 128);
+  text(title, extraSpaceW + mainSpace/2, extraSpaceH + margin*3);
+
+  stroke(0, 50);
+  strokeWeight(1);
+  line(extraSpaceW + margin*2, extraSpaceH + margin*3 + 30, width - margin*2, extraSpaceH + margin*3 + 30);
+
+  textAlign(CENTER, TOP);
+  stroke(0, 150);
+  strokeWeight(1);
+  textSize(20);
+  fill(0, 150);
+  text(artist, extraSpaceW + mainSpace/2, extraSpaceW + margin*4 + 30);
+
+  // line(0, cursorTop, width, cursorTop);
+  // line(0, cursorBottom, width, cursorBottom);
+
+  navBox.displayBack();
+
+  if (loaded) {
+    navCursor.update();
+    navCursor.display();
+    clock.display();
+  }
+
   if (!paused) {
+    currentTime = track.currentTime();
+
     var x = str(track.currentTime().toFixed(2));
     var p = pitchTrack[x];
     if (p >= minHz && p <= maxHz) {
-      var y = map(p, minHz, maxHz, height-15, 110);
-      print(x, p, y);
-      noFill();
-      stroke(0);
-      strokeWeight(1);
-      ellipse(extraSpaceW+mainSpace/2, y, 3, 3);
+      var y = map(p, minHz, maxHz, buttonPlay.y-margin*2, extraSpaceH + margin*5 + 50);
+      // print(x, p, y);
+      noStroke();
+      // fill(120, 0, 0);
+      fill("red");
+      // noFill();
+      // stroke(0);
+      // strokeWeight(1);
+      ellipse(extraSpaceW+mainSpace/2, y, 5, 5);
+    }
+  }
+
+  navBox.displayFront();
+}
+
+function createNavigationBox () {
+  this.x1 = extraSpaceW + margin;
+  this.x2 = width - margin;
+  this.y1 = height - margin - navBoxH;
+  this.y2 = height - margin;
+  this.w = this.x2 - this.x1;
+
+  this.displayBack = function () {
+    fill(0, 50);
+    noStroke();
+    rect(this.x1, this.y1, this.w, navBoxH);
+  }
+
+  this.displayFront = function () {
+    stroke(0, 150);
+    strokeWeight(2);
+    line(this.x1+1, this.y1, this.x2, this.y1);
+    line(this.x2, this.y1, this.x2, this.y2);
+    strokeWeight(1);
+    line(this.x1, this.y1, this.x1, this.y2);
+    line(this.x1, this.y2, this.x2, this.y2);
+  }
+
+  this.clicked = function () {
+    if (mouseX > this.x1 && mouseX < this.x2 && mouseY > this.y1 && mouseY < this.y2) {
+      jump = map(mouseX, this.x1, this.x2, 0, trackDuration);
+      if (paused) {
+        currentTime = jump;
+      } else {
+        track.jump(jump);
+        jump = undefined;
+      }
     }
   }
 }
@@ -82,12 +177,55 @@ function start () {
   currentTime = 0;
   var currentRecording = recordingsInfo[recordingsList[select.value()]];
   trackFile = currentRecording.info.trackFile;
+  title = currentRecording.info.title;
+  artist = currentRecording.info.artist;
+  link = currentRecording.info.link;
+  infoLink.attribute("href", link)
+    .html("+info");
+  trackDuration = currentRecording.info.duration;
   pitchSpace = currentRecording.melody.pitchSpace;
   minHz = pitchSpace[0].pitch;
   maxHz = pitchSpace[pitchSpace.length-1].pitch;
   pitchTrack = currentRecording.melody.pitchTrack;
+  clock = new CreateClock;
   buttonPlay.html("Carga el audio");
   buttonPlay.removeAttribute("disabled");
+}
+
+function CreateNavCursor () {
+  this.x = navBox.x1 + navCursorW/2;
+
+  this.update = function () {
+    this.x = map(currentTime, 0, trackDuration, navBox.x1+navCursorW/2, navBox.x2-navCursorW/2);
+    if (navBox.x2 - navCursorW/2 - this.x < 0.005) {
+      buttonPlay.html("¡Comienza!");
+      track.stop();
+      paused = true;
+      currentTime = 0;
+    }
+  }
+
+  this.display = function () {
+    stroke(120, 0, 0);
+    strokeWeight(navCursorW);
+    line(this.x, navBox.y1+navCursorW/2, this.x, navBox.y2-navCursorW/2);
+  }
+}
+
+function CreateClock () {
+  this.clock;
+  this.total = niceTime(trackDuration);
+  this.now;
+  this.display = function () {
+    this.now = niceTime(currentTime);
+    this.clock = this.now + " / " + this.total;
+    textAlign(CENTER, BOTTOM);
+    textSize(12);
+    textStyle(NORMAL);
+    noStroke();
+    fill(50);
+    text(this.clock, extraSpaceW+mainSpace/2, buttonPlay.y+buttonPlay.height);
+  }
 }
 
 function player () {
@@ -128,4 +266,18 @@ function soundLoaded () {
 
 function failedLoad () {
   print("Loading failed");
+}
+
+function mouseClicked () {
+  if (loaded) {
+    navBox.clicked();
+  }
+}
+
+function niceTime (seconds) {
+  var niceTime;
+  var sec = int(seconds%60);
+  var min = int(seconds/60);
+  niceTime = str(min).padStart(2, "0") + ":" + str(sec).padStart(2, "0");
+  return niceTime
 }
