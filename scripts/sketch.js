2738f8a7a79f2463807ad4319116a71384f9d1ee
Rafael Caro Repetto <rafael.caro.repetto@gmail.com>
2019-01-22 10:00:57 +0100

    First working version of svara display (ugly)

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -3,9 +3,17 @@ var extraSpaceW = 0;
 var mainSpace = 600;
 var margin = 10;
 var easing = 0.1;
+var backColor;
+var frontColor;
 
 var recordingsInfo;
 var recordingsList;
+var pitchSpace;
+var svaraList = [];
+var svaraRadius1 = 19;
+var svaraRadius2 = 17;
+var svaraRadius3 = 15;
+var svaraLine = 30;
 var minHz;
 var maxHz;
 var pitchTrack;
@@ -49,6 +57,9 @@ function setup () {
   textFont("Laila");
   strokeJoin(ROUND);
 
+  backColor = color(240, 128, 128);
+  frontColor = color(120, 0, 0);
+
   recordingsList = recordingsInfo["recordingsList"];
 
   infoLink = select("#info-link");
@@ -77,19 +88,19 @@ function setup () {
   navBox = new createNavigationBox();
   navCursor = new CreateNavCursor();
 
-  cursorTop = extraSpaceH + margin*6 + 50;
+  cursorTop = extraSpaceH + margin*7 + 50;
   cursorBottom = buttonPlay.y-margin*2;
 }
 
 function draw () {
-  background(240, 128, 128);
+  background(backColor);
 
   textAlign(CENTER, TOP);
   textStyle(NORMAL);
   textSize(30);
   strokeWeight(5);
-  stroke(120, 0, 0);
-  fill(240, 128, 128);
+  stroke(frontColor);
+  fill(backColor);
   text(title, extraSpaceW + mainSpace/2, extraSpaceH + margin*3);
 
   stroke(0, 50);
@@ -105,6 +116,10 @@ function draw () {
 
   // line(0, cursorTop, width, cursorTop);
   // line(0, cursorBottom, width, cursorBottom);
+
+  for (var i = 0; i < svaraList.length; i++) {
+    svaraList[i].display();
+  }
 
   navBox.displayBack();
 
@@ -172,6 +187,80 @@ function createNavigationBox () {
   }
 }
 
+function CreateNavCursor () {
+  this.x = navBox.x1 + navCursorW/2;
+
+  this.update = function () {
+    this.x = map(currentTime, 0, trackDuration, navBox.x1+navCursorW/2, navBox.x2-navCursorW/2);
+    if (navBox.x2 - navCursorW/2 - this.x < 0.005) {
+      buttonPlay.html("¡Comienza!");
+      track.stop();
+      paused = true;
+      currentTime = 0;
+    }
+  }
+
+  this.display = function () {
+    stroke(frontColor);
+    strokeWeight(navCursorW);
+    line(this.x, navBox.y1+navCursorW/2, this.x, navBox.y2-navCursorW/2);
+  }
+}
+
+function CreateSvara (svara) {
+  this.x1 = extraSpaceW + mainSpace/2;
+  this.y = map(svara.pitch, minHz, maxHz, cursorBottom, cursorTop);
+  this.name = svara.svara;
+  this.function = svara.function;
+  if (this.function == "sadja") {
+    this.radius = svaraRadius1;
+    this.col = frontColor;
+    this.txtCol = backColor;
+    this.txtStyle = BOLD;
+  } else if (this.function == "vadi") {
+    this.radius = svaraRadius1;
+    this.col = backColor;
+    this.txtCol = frontColor;
+    this.txtStyle = BOLD;
+  } else if (this.function == "samvadi") {
+    this.radius = svaraRadius2;
+    this.col = backColor;
+    this.txtCol = frontColor;
+    this.txtStyle = NORMAL;
+  } else {
+    this.radius = svaraRadius3;
+    this.col = backColor;
+    this.txtCol = frontColor;
+    this.txtStyle = NORMAL;
+  }
+  if (svaraList.length == 0) {
+    this.position = 0;
+  } else if (svaraList[svaraList.length-1].position == 0) {
+    this.position = 1;
+  } else {
+    this.position = 0;
+  }
+  this.x2 = this.x1 + svaraLine/2 + svaraRadius1 + (svaraRadius1*2 + margin) * this.position;
+
+  this.display = function () {
+    stroke(frontColor);
+    strokeWeight(1);
+    line(this.x1-svaraLine/2, this.y, this.x2, this.y)
+
+    stroke(frontColor);
+    strokeWeight(2);
+    fill(this.col);
+    ellipse(this.x2, this.y, this.radius, this.radius);
+
+    textAlign(CENTER, CENTER);
+    noStroke();
+    textSize(this.radius*0.8);
+    textStyle(this.txtStyle);
+    fill(this.txtCol);
+    text(this.name, this.x2, this.y);
+  }
+}
+
 function start () {
   if (loaded) {
     track.stop();
@@ -189,30 +278,14 @@ function start () {
   pitchSpace = currentRecording.melody.pitchSpace;
   minHz = pitchSpace[0].pitch;
   maxHz = pitchSpace[pitchSpace.length-1].pitch;
+  for (var i = 0; i < pitchSpace.length; i++) {
+    var svara = new CreateSvara(pitchSpace[i]);
+    svaraList.push(svara);
+  }
   pitchTrack = currentRecording.melody.pitchTrack;
   clock = new CreateClock;
   buttonPlay.html("Carga el audio");
   buttonPlay.removeAttribute("disabled");
-}
-
-function CreateNavCursor () {
-  this.x = navBox.x1 + navCursorW/2;
-
-  this.update = function () {
-    this.x = map(currentTime, 0, trackDuration, navBox.x1+navCursorW/2, navBox.x2-navCursorW/2);
-    if (navBox.x2 - navCursorW/2 - this.x < 0.005) {
-      buttonPlay.html("¡Comienza!");
-      track.stop();
-      paused = true;
-      currentTime = 0;
-    }
-  }
-
-  this.display = function () {
-    stroke(120, 0, 0);
-    strokeWeight(navCursorW);
-    line(this.x, navBox.y1+navCursorW/2, this.x, navBox.y2-navCursorW/2);
-  }
 }
 
 function CreateClock () {
